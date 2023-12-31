8ea9b589da163d539b24ebc63c96c951ad00dc99
joseph <wdg0434@gmail.com>
2023-12-31 14:40:00 -0500

    feat/22 I encrypt DB id and add/Delete some comments

--- src/main/java/GHOST/sk_ghost/config/JasyptConfig.java
+++ src/main/java/GHOST/sk_ghost/config/JasyptConfig.java
@@ -17,20 +17,19 @@ public class JasyptConfig {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         encryptor.setProvider(new BouncyCastleProvider()); //Bouncy Castle 라이브러리에서 제공하는 암호화 알고리즘들을 생성하여 암호화에 적용
         encryptor.setPoolSize(2); // 암호화 및 복호화 작업을 수행하는 데 사용하는 스레드 풀의 크기(2를 권장)
-        encryptor.setPassword("password"); // 암호화 키 -> 암호화 및 복호화에 사용되는 키.. 실제 운영 환경에서는 이 password 도 가려야 함.
+        encryptor.setPassword("password"); // 암호화 키 -> 암호화 및 복호화에 사용되는 키.. 실제 운영 환경에서는 이 password 도 가려야 함. (추후 txt파일에서 불러오기)
         encryptor.setAlgorithm("PBEWithSHA256And128BitAES-CBC-BC"); // Bouncy Castle 라이브러리에서 제공하는 암호화 알고리즘들 중에서 사용할 알고리즘(PBE~)를 세팅
 
-//        String db_password = "ghost123";
-//        String encrypted_db_password = encryptor.encrypt(db_password);
-//        System.out.println("Enc = " + encrypted_db_password);
+//      실제 암호화 수행
+//      주의 : 암호화 수행 시 암호화된 코드가 변경되므로 동일 텍스트에 대해 두번수행하지 않도록 해야 합니다
+//      String db_text = "123"; //123은 암호화하고자 하는 문자열 예시
+//      String encrypted_db_text = encryptor.encrypt(db_text);
+//      System.out.println("Enc = " + encrypted_db_text);
+//
+//      암호화한 db_text 복호화 및 정상출력 확인
+//      String decryptedText = encryptor.decrypt(encrypted_db_text);
+//      System.out.println("Dec = " + decryptedText);
 
-        //테스트 코드 시작 (추후 삭제)
-//        String plainText = "ghost123";
-//        String encryptedText = encryptor.encrypt(plainText);
-//        String decryptedText = encryptor.decrypt(encryptedText);
-//        System.out.println("Enc = " + encryptedText);
-//        System.out.println("Dec = " + decryptedText);
-        //테스트 코드 끝
 
         return encryptor;
     }
