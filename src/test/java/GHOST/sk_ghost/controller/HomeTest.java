ef6821a5edc1e5953f4efddb3915634c8999272a
joseph <wdg0434@gmail.com>
2023-12-31 18:00:40 -0500

    feat/28-junit-check-unittest

--- src/test/java/GHOST/sk_ghost/controller/HomeTest.java
+++ src/test/java/GHOST/sk_ghost/controller/HomeTest.java
@@ -5,21 +5,26 @@ import GHOST.sk_ghost.service.V1service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootTest(classes = SkGhostApplication.class) //SpringBootTest() 에 파라미터로 어떤 Spring 어플리케이션을 테스트하는지 명시한다.
 @AutoConfigureMockMvc //
 class HomeTest {
     @Autowired    //Autowired를 이용해 필요한 빈을 주입받을 수 있다.
     private MockMvc mockMvc;
-    private V1service v1service;
+    @Autowired
+    private V1service v1service; //new
+    @Autowired
+    private Home homecontroller; //new
 
     @Test
     public void test_doHome() throws Exception {
@@ -34,15 +39,40 @@ class HomeTest {
     }
 
     @Test
-    void checkOfUser() throws Exception {
-        MvcResult result = mockMvc.perform(get("/"))
+    void test_checkOfUser() throws Exception {
+
+        String requestBody = "{\"Who\":\"jpwoo327@kakao.com\"}";
+
+        // POST 요청 수행
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/check")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn();
 
-        String content = result.getResponse().getContentAsString();
-
-        System.out.println("Test checkOfUser is good");
+        // 결과 확인
+        String content = new String(result.getResponse().getContentAsByteArray(), StandardCharsets.UTF_8);
         System.out.println(content);
+        System.out.println("Test checkOfUser_validUser is good");
+
+//        V1service v1serviceMock = mock(V1service.class);
+        // 특정 메서드가 호출될 때 반환할 값을 지정
+//        when(v1service.userList()).thenReturn(Collections.singletonList(
+//                Collections.singletonMap("id", "validUserId")
+//        ));
+//        // HTTP 요청 수행
+//        MvcResult result = (MvcResult) mockMvc.perform(MockMvcRequestBuilders.post("/check")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content("{\"Who\":\"validUserId\"}"))
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andExpect(MockMvcResultMatchers.content().string("ValidUser validUserId"))
+//                .andReturn();
+//
+//        String content = result.getResponse().getContentAsString();
+//
+//        System.out.println("Test checkOfUser_validUser is good");
+//        System.out.println(content);
     }
 
     @Test
