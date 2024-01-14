979764e761feae2dbfd40914b65a787a35102bdc
joseph <wdg0434@gmail.com>
2024-01-14 18:28:22 -0500

    refac/91

--- src/main/resources/static/JavaScript/deleteScheduleFunction.js
+++ src/main/resources/static/JavaScript/deleteScheduleFunction.js
@@ -1,12 +1,24 @@
 function deleteScheduleFunction() {
+    loadingOn()
     deleteOffBtn()
     let userId;
     let userImage;
     let userName
-    let noFlag = false; // 수정할 페이지가 하나도 없다면 false
+    let noFlag = true; // 수정할 페이지가 하나도 없다면 false
+
+    let nowUserId = null;
+    let nowUserNiname = null;
+    let tableSelect = null;
+    let rocMembers = null; //Whatfor
+    let flag  = true; //What for
+    let noChange = true; //What for
+    let deleteInfo = []; // "A" > ""
+    let firstClick= 0;
+    let clickFlag = -1; // 1 인경우 : 선택된 상태,  -1인 경우 : 미선택된 상태
 
     // 사용자가 OP 페이지에 들어오면 카카오톡 프로필, 이름을 애니메이션으로 보여준다.
     Kakao.Auth.getStatusInfo(function(statusObj) {
+        //로그인 성공 시
         if (statusObj.status === 'connected') {
             userId = statusObj.user.kakao_account.email;
             userName = statusObj.user.properties.nickname
@@ -29,75 +41,52 @@ function deleteScheduleFunction() {
             let cards = getDates(startDate, endDate); // ['2023-01-01', ... '2024-01-07']
             console.log(cards)
 
-            // 순차적으로 비동기 작업을 수행하는 함수
-            async function processCards() {
-                for (let card of cards) {
-                    await processCard(card);
+            let xhr1 = new XMLHttpRequest();
+            let payloadFront = {"date": cards};
+            xhr1.open('POST', '/getSchedule', true);
+            xhr1.setRequestHeader("Content-Type", "application/json");
+            xhr1.send(JSON.stringify(payloadFront));
+
+            xhr1.onload = function () {
+                let results = JSON.parse(xhr1.response); // 디비에서 해당 날짜의 운영자가 하나도 없는 경우 [] 반환
+                console.log(results);
+
+                for(let i = 0; i < results.length; i++) {
+                    if (results[i].length !== 0) {
+                        noFlag = false;
+                        break;
+                    }
                 }
 
-                if (!noFlag) {
-                    window.location.href ='/admin?id='+userId
-                    deleteOnBtn()
-                    alert("현재 날짜 범위는 스케줄이 없습니다.")
-                }
-                else {
-                    afterProcessCards() //카드 DOM 으로 생성 후 작업
-                }
-                deleteOnBtn()
-            }
-
-            // 각 날짜에 대한 비동기 작업을 수행하는 함수
-            function processCard(card) {
-                return new Promise(resolve => {
-                    let xhr1 = new XMLHttpRequest();
-                    let payloadFront = {"date": card};
-                    xhr1.open('POST', '/getSchedule', true); // 날짜에 맞는 스케줄 정보 (이름, 날짜, shift, 우선순위, 공정)
-                    xhr1.setRequestHeader("Content-Type", "application/json");
-                    xhr1.send(JSON.stringify(payloadFront));
-
-                    xhr1.onload = function () {
-                        let results = JSON.parse(xhr1.response);
-                        console.log(results);
-
-                        // 요일 생성 부분, card는 날짜임
+                if (noFlag) {
+                    let idx = 0
+                    for (let card of cards) {
                         const dateObject = new Date(`${card}T00:00:00-05:00`);
                         const options = {weekday: 'short', timeZone: 'America/New_York'};
                         const dayOfWeek = dateObject.toLocaleDateString('en-US', options);
 
-                        if (results.length !== 0) { //해당 날짜에 대한 값이 없으면 카드 생성하지 않음
-                            noFlag = true //
-                            deleteScheduleCard(results, dayOfWeek, card); // DOM 에서 동적으로 카드를 생성하여 부모 컨테이너에 삽입
-                        }
-
-                        resolve();
+                        // DOM 에서 동적으로 카드르 만들어 배열에 저장. 붙이기는 정렬 후 한번에 수행하게 됨.
+                        deleteScheduleCard(results[idx], dayOfWeek, card);
+                        idx += 1
                     }
-                });
-            }
+                    loadingOff()
+                    deleteOnBtn()
+                    // 여기서 카드 생성 끝
 
-            // 비동기 작업을 순차적으로 실행 > 모든 카드가 DOM에 생성됨
-            processCards();
+                }
+                else {
+                    alert("삭제할 카드가 없습니다.")
+                    loadingOff()
+                    deleteOnBtn()
+                    window.location.href = "/admin?id=" + nowUserId
 
-            function afterProcessCards() {
-                // 버튼 요소 가져오기
-                let button1 = document.getElementById('delete_save');
-
-                // 클릭 이벤트에 리스너 등록
-                button1.addEventListener('click', function() {
-                    deleteSaveBtn() // 자체 개발 필요, 개발2
-                });
-
-                // 버튼 요소 가져오기
-                let button2 = document.getElementById('delete_cancel');
-
-                // 클릭 이벤트에 리스너 등록
-                button2.addEventListener('click', function() {
-                    deleteCancelBtn() // 그대로 쓰면 됨. id만 바꾸기
-                });
-
+                }
             }
         }
+        // 로그인 실패 시
         else {
             deleteOnBtn()
+            loadingOff()
             window.location.href = "/"
             alert("세션이 만료되었습니다. 로그인을 다시해주세요.")
         }
