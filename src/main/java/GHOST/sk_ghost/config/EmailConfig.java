fa990c3a41ed763ac9d1317bf4687e428783b1a4
joseph <wdg0434@gmail.com>
2024-01-16 23:04:50 -0500

    fix/111-EmailAuthSystem

--- src/main/java/GHOST/sk_ghost/config/EmailConfig.java
+++ src/main/java/GHOST/sk_ghost/config/EmailConfig.java
@@ -1,56 +1,33 @@
 //package GHOST.sk_ghost.config;
 //
-//import java.util.Properties;
-//
-//import org.springframework.beans.factory.annotation.Value;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.PropertySource;
 //import org.springframework.mail.javamail.JavaMailSender;
 //import org.springframework.mail.javamail.JavaMailSenderImpl;
 //
-//
+//import java.util.Properties;
 //@Configuration
-//@PropertySource("classpath:email.properties")
 //public class EmailConfig {
-//
-//    @Value("${mail.smtp.port}")
-//    private int port;
-//    @Value("${mail.smtp.socketFactory.port}")
-//    private int socketPort;
-//    @Value("${mail.smtp.auth}")
-//    private boolean auth;
-//    @Value("${mail.smtp.starttls.enable}")
-//    private boolean starttls;
-//    @Value("${mail.smtp.starttls.required}")
-//    private boolean startlls_required;
-//    @Value("${mail.smtp.socketFactory.fallback}")
-//    private boolean fallback;
-//    @Value("${AdminMail.id}")
-//    private String id;
-//    @Value("${AdminMail.password}")
-//    private String password;
-//
 //    @Bean
-//    public JavaMailSender javaMailService() {
-//        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-//        javaMailSender.setHost("smtp.gmail.com");
-//        javaMailSender.setUsername(id);
-//        javaMailSender.setPassword(password);
-//        javaMailSender.setPort(port);
-//        javaMailSender.setJavaMailProperties(getMailProperties());
-//        javaMailSender.setDefaultEncoding("UTF-8");
-//        return javaMailSender;
-//    }
-//    private Properties getMailProperties()
-//    {
-//        Properties pt = new Properties();
-//        pt.put("mail.smtp.socketFactory.port", socketPort);
-//        pt.put("mail.smtp.auth", auth);
-//        pt.put("mail.smtp.starttls.enable", starttls);
-//        pt.put("mail.smtp.starttls.required", startlls_required);
-//        pt.put("mail.smtp.socketFactory.fallback",fallback);
-//        pt.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-//        return pt;
+//    public JavaMailSender mailSender() {//JAVA MAILSENDER 인터페이스를 구현한 객체를 빈으로 등록하기 위함.
+//
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();//JavaMailSender 의 구현체를 생성하고
+//        mailSender.setHost("smtp.gmail.com");// 속성을 넣기 시작합니다. 이메일 전송에 사용할 SMTP 서버 호스트를 설정
+//        mailSender.setPort(587);// 587로 포트를 지정
+//        mailSender.setUsername("ghostserviceteam.official@gmail.com");//구글계정을 넣습니다.
+//        mailSender.setPassword("ojlmaepmjaqbpmsw");//구글 앱 비밀번호를 넣습니다.
+//
+//        Properties javaMailProperties = new Properties();//JavaMail의 속성을 설정하기 위해 Properties 객체를 생성
+//        javaMailProperties.put("mail.transport.protocol", "smtp");//프로토콜로 smtp 사용
+//        javaMailProperties.put("mail.smtp.auth", "true");//smtp 서버에 인증이 필요
+//        javaMailProperties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");//SSL 소켓 팩토리 클래스 사용
+//        javaMailProperties.put("mail.smtp.starttls.enable", "true");//STARTTLS(TLS를 시작하는 명령)를 사용하여 암호화된 통신을 활성화
+//        javaMailProperties.put("mail.debug", "true");//디버깅 정보 출력
+//        javaMailProperties.put("mail.smtp.ssl.trust", "smtp.naver.com");//smtp 서버의 ssl 인증서를 신뢰
+//        javaMailProperties.put("mail.smtp.ssl.protocols", "TLSv1.2");//사용할 ssl 프로토콜 버젼
+//
+//        mailSender.setJavaMailProperties(javaMailProperties);//mailSender에 우리가 만든 properties 넣고
+//
+//        return mailSender;//빈으로 등록한다.
 //    }
 //}
